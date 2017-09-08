462be6f69ea059a327d494c5df786307d067ba92
danielromanich <daniel@romanich.com>
2017-09-08 13:57:04 +0200

    Changed the port the server uses

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html" ));
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8082, function () {
+  console.log('Server has started, listening on port 8082')
 });
