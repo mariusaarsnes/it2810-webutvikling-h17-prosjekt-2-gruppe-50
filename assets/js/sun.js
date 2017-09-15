91c8ee94bb4469c2d64c8792ef97e74871ba337d
Westermoen <jakob@westermoen.com>
2017-09-15 12:00:12 +0200

    added files for sun HTML

---
+++ assets/js/sun.js
@@ -0,0 +1,38 @@
+function play() {
+  document.getElementById('player').play();
+}
+
+function pause() {
+  document.getElementById('player').pause();
+}
+
+function setVolume() {
+  var audio = document.getElementById('player');
+  audio.volume = document.getElementById('fader').value;
+}
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
