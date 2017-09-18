8b4a7f52df301f1ba18e6b0485411c43ce543897
Marius Aarsnes <marius.aarsnes@gmail.com>
2017-09-18 13:48:02 +0200

    Updating Ajax to work with class instead of ID

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,18 +1,18 @@
 var dict = {
-    "nav-home-button": "home.html",
-    "nav-dog-button": "dog.html",
-    "nav-travel-button": "travel.html",
-    "nav-sun-button": "sun.html",
-    "nav-beer-button": "beer.html",
-    "nav-eddy-button": "eddy.html"
+    "home-button": "home.html",
+    "dog-button": "dog.html",
+    "travel-button": "travel.html",
+    "sun-button": "sun.html",
+    "beer-button": "beer.html",
+    "eddy-button": "eddy.html",
+    "button": "documentation.html"
 };
 
 
 $("a").click(function(e) {
-    var temp = e.target.id;
-    console.log(temp);
+    var temp = e.className.split(" ")[0];
     $.get("assets/content/"+ dict[temp], function(data) {
-        $("#pagecontent").html(data)
+        $("#pagecontent").html(data);
         if (temp == "nav-beer-button") {
             initiateBubbles($(".beer-bubble"));
         }
